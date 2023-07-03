3a32e52ad30c72fbdd70b60e78fce5fe3c02ee18
chungddong <chungman010918@gmail.com>
2023-07-04 00:04:38 +0900

    디자인 수정 및 timeline요소 추가

--- window_disaster_noti/main.xaml.cs
+++ window_disaster_noti/main.xaml.cs
@@ -22,10 +22,12 @@ namespace window_disaster_noti
     /// </summary>
     public partial class main : Window
     {
-        DispatcherTimer timer = new DispatcherTimer();
-        private List<noti> list;
+        DispatcherTimer timer = new DispatcherTimer(); //타이머 선언
+        private List<noti> list; //이전 메시지들 모음을 위한 리스트 
 
-        private string lastnum;
+        private string lastnum; //가장 최근의 재난문자 ID
+
+        public string url = "https://www.safekorea.go.kr/idsiSFK/sfk/cs/sua/web/DisasterSmsList.do"; //재난문자 데이터 소스링크
 
         public main()
         {
@@ -38,38 +40,36 @@ namespace window_disaster_noti
 
             timer.Tick += new EventHandler(timer_Tick);          //이벤트 추가
 
-            timer.Start();
+            //timer.Start(); //타이머 시작 - 메인 이벤트 막으려면 주석처리
 
         }
 
         private async void timer_Tick(object sender, EventArgs e)
         {
-
-            string url = "https://www.safekorea.go.kr/idsiSFK/sfk/cs/sua/web/DisasterSmsList.do";
-            string payloadData = "{\"searchInfo\":{\"pageIndex\":\"1\",\"pageUnit\":\"10\",\"pageSize\":\"10\",\"firstIndex\":\"1\",\"lastIndex\":\"1\",\"recordCountPerPage\":\"10\",\"searchBgnDe\":\"2023-06-25\",\"searchEndDe\":\"2023-06-27\",\"searchGb\":\"1\",\"searchWrd\":\"\",\"rcv_Area_Id\":\"\",\"dstr_se_Id\":\"\",\"c_ocrc_type\":\"\",\"sbLawArea1\":\"\",\"sbLawArea2\":\"\",\"sbLawArea3\":\"\"}}";
+            string payloadData = "{\"searchInfo\":{\"pageIndex\":\"1\",\"pageUnit\":\"10\",\"pageSize\":\"10\",\"firstIndex\":\"1\",\"lastIndex\":\"1\",\"recordCountPerPage\":\"10\",\"searchBgnDe\":\"2023-07-01\",\"searchEndDe\":\"2023-07-03\",\"searchGb\":\"1\",\"searchWrd\":\"\",\"rcv_Area_Id\":\"\",\"dstr_se_Id\":\"\",\"c_ocrc_type\":\"\",\"sbLawArea1\":\"\",\"sbLawArea2\":\"\",\"sbLawArea3\":\"\"}}";
 
             string boardContent = await GetBoardContent(url, payloadData);
 
 
-            JObject jobject = JObject.Parse(boardContent);
+            JObject jobject = JObject.Parse(boardContent); //jobject 형태로 payloadData 가져오기(json데이터)
 
             //List<DisData> disData = JsonConvert.DeserializeObject<List<DisData>>(boardContent);
 
-            var newitem = new noti { Title = "" + jobject["disasterSmsList"][0]["MD101_SN"], maintext = "" + jobject["disasterSmsList"][0]["MSG_CN"] };
+            var newitem = new noti { Title = "" + jobject["disasterSmsList"][0]["DSSTR_SE_NM"] + " - " + jobject["disasterSmsList"][0]["RCV_AREA_NM"], maintext = "" + jobject["disasterSmsList"][0]["MSG_CN"], timeline = "" + jobject["disasterSmsList"][0]["REGIST_DT"]};
 
-            if("" + jobject["disasterSmsList"][0]["MD101_SN"] != lastnum)
+            if("" + jobject["disasterSmsList"][0]["MD101_SN"] != lastnum) //가장 최근의 재난문자 id 와 다르면 새로운 메시지를 추가
             {
                 lastnum = "" + jobject["disasterSmsList"][0]["MD101_SN"];
                 lstbox.Items.Add(newitem);
             }
             
 
-            timer.Start();
+            timer.Start(); //재귀 실행
 
 
         }
 
-        async Task<string> GetBoardContent(string requestUrl, string payloadData)
+        async Task<string> GetBoardContent(string requestUrl, string payloadData)  //url로부터 json데이터 가져오는 코드
         {
             using (HttpClient client = new HttpClient())
             {
@@ -94,10 +94,12 @@ namespace window_disaster_noti
         }
     }
 
-    public class noti
+    public class noti  //메시지 출력용 구조 선언
     {
-        public string Title { get; set; }
+        public string Title { get; set; } //제목
 
-        public string maintext { get; set; }
+        public string maintext { get; set; } //내용
+
+        public string timeline { get; set; } //시간
     }
 }
