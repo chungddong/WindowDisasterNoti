b6b8e9d17001907b834217f7af4f6bfe9eae1164
chungddong <chungman010918@gmail.com>
2023-08-27 01:17:19 +0900

    안쓰는 코드 정리

--- window_disaster_noti/App.xaml.cs
+++ window_disaster_noti/App.xaml.cs
@@ -15,30 +15,9 @@ namespace window_disaster_noti
     /// </summary>
     public partial class App : Application
     {
-        Winforms.NotifyIcon noti;
 
         private void Onstartup(object sender, StartupEventArgs e)
         {
-            //setNotiTray();
-            /*base.OnStartup(e);
-            setNotiTray();
-            if (noti == null)
-            {
-                setNotiTray();
-            }*/
-        }
-
-        private void setNotiTray()
-        {
-            noti = new Winforms.NotifyIcon();
-            //noti.Icon = new System.Drawing.Icon("icon.png");
-            var stream = GetResourceStream(new Uri("pack://application:,,,/window_disaster_noti;component/icon.png")).Stream;
-            var bitmap = new Bitmap(stream);
-            noti.Icon = Icon.FromHandle(bitmap.GetHicon());
-            noti.Visible = true;
-            noti.Text = "NotiTest";
-
-            
         }
     }
 
